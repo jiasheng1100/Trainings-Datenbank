74e02933314cb1eeb63e3c73d9b90b58630cfa82
Jia Sheng <shengjia1100@gmail.com>
2022-07-01 16:25:58 +0200

    added a few sql retrieval commands

--- Abfragen.sql
+++ Abfragen.sql
@@ -12,3 +12,45 @@ from training
 join trainieren on training.id = trainieren.training
 join aufzeichnung on trainieren.aufzeichnung = aufzeichnung.id
 where aufzeichnung.besitztvon=01;
+
+--Anzahl und Gesamtdauer der Trainings pro Monat
+select count(*), sum(Dauer)
+from aufzeichnung
+where besitztvon=01
+and extract(year from datum) = 2014
+group by date_trunc('month', datum);
+
+-- Balkendiagramm: Anzahl der Trainings pro Monat
+select date_trunc('month', datum)
+as Monat,
+count(*)
+from aufzeichnung
+where besitztvon=01
+and extract(year from datum) = 2014
+group by date_trunc('month', datum);
+
+-- Tortendiagramm: Anzahl der Trainings pro Monat
+select cast(date_trunc('month', datum) as Text)
+as Monat,
+count(*)
+from aufzeichnung
+where besitztvon=01
+and extract(year from datum) = 2014
+group by date_trunc('month', datum);
+
+--Tortendiagramm: Anzahl der Trainings jeder Art
+select art, count(*)
+from aufzeichnung
+where besitztvon=01
+and extract(year from datum) = 2014
+group by art;
+
+--Tortendiagramm: Prozent aller Trainings
+select training.name, count(trainieren.*)
+from training
+join trainieren on training.id = trainieren.training
+join aufzeichnung on trainieren.aufzeichnung = aufzeichnung.id
+where aufzeichnung.besitztvon=01
+group by(training.id);
+
+
